f63ae0b724d0fbfe6ad55a0e6dc8dcfeec35ffd5
Loma <909622852@163.com>
2019-08-06 15:07:50 +0800

    first commit

---
+++ .next/static/development/pages/b.js
@@ -0,0 +1,79 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\b.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fb&absolutePagePath=C%3A%5CUsers%5Cqduser02%5CDesktop%5Cdemo%5Ccustom-server-app%5Cpages%5Cb.js!./":
+/*!*****************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fb&absolutePagePath=C%3A%5CUsers%5Cqduser02%5CDesktop%5Cdemo%5Ccustom-server-app%5Cpages%5Cb.js ***!
+  \*****************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/b", function() {
+      var page = __webpack_require__(/*! ./pages/b.js */ "./pages/b.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/b.js */ "./pages/b.js", function() {
+          if(!next.router.components["/b"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/b.js */ "./pages/b.js")
+          next.router.update("/b", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_7aff549c98b978433226 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_7aff549c98b978433226 */ "dll-reference dll_7aff549c98b978433226"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/b.js":
+/*!********************!*\
+  !*** ./pages/b.js ***!
+  \********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "b");
+});
+
+/***/ }),
+
+/***/ 1:
+/*!*********************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fb&absolutePagePath=C%3A%5CUsers%5Cqduser02%5CDesktop%5Cdemo%5Ccustom-server-app%5Cpages%5Cb.js ***!
+  \*********************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fb&absolutePagePath=C%3A%5CUsers%5Cqduser02%5CDesktop%5Cdemo%5Ccustom-server-app%5Cpages%5Cb.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fb&absolutePagePath=C%3A%5CUsers%5Cqduser02%5CDesktop%5Cdemo%5Ccustom-server-app%5Cpages%5Cb.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_7aff549c98b978433226":
+/*!*******************************************!*\
+  !*** external "dll_7aff549c98b978433226" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_7aff549c98b978433226;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=b.js.map
